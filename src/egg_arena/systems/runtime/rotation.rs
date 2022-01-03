73fe2e5aeebdba8a43d83399c5928d8f41ece479
blipovac <bruno.lipovac1011@gmail.com>
2022-01-03 21:54:59 +0100

    Add a rotation system

---
+++ src/egg_arena/systems/runtime/rotation.rs
@@ -0,0 +1,11 @@
+use bevy::prelude::{Res, Time, Query, Transform, With, Quat};
+
+use crate::egg_arena::components::Rotates;
+
+pub fn rotate(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
+    for mut transform in query.iter_mut() {
+        *transform = Transform::from_rotation(Quat::from_rotation_y(
+            (4.0 * std::f32::consts::PI / 20.0) * time.delta_seconds(),
+        )) * *transform;
+    }
+}
