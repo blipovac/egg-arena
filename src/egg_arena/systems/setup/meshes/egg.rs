7f2ac986a6e6b312f2862e7300a899899d72095f
blipovac <othermmo@gmail.com>
2022-01-29 21:30:26 +0100

    Spawn scene instead of individual gltf components

--- src/egg_arena/systems/setup/meshes/egg.rs
+++ src/egg_arena/systems/setup/meshes/egg.rs
@@ -1,9 +1,6 @@
-use bevy::prelude::{
-    shape, AssetServer, Assets, Color, Commands, Handle, Mesh, PbrBundle,
-    Query, Res, ResMut, StandardMaterial, Image, Transform, With,
-};
+use bevy::prelude::*;
 
-use crate::egg_arena::components::{EggCount};
+use crate::egg_arena::components::EggCount;
 use crate::egg_arena::items::Game;
 
 pub fn setup(
@@ -17,35 +14,21 @@ pub fn setup(
     let egg_column_count: u32 = 2;
     let egg_padding: f32 = 2.0;
 
-    let egg_handle: Handle<Mesh> = asset_server.load("egg/scene.gltf#Mesh0/Primitive0");
-
-    let egg_texture_handle: Handle<Image> =
-        asset_server.load("egg/textures/DefaultMaterial_baseColor.jpeg");
-
-    let egg_metallic_roughness_texture_handle: Handle<Image> =
-        asset_server.load("egg/textures/DefaultMaterial_metallicRoughness.png");
-
-    let egg_normal_map_handle: Handle<Image> =
-        asset_server.load("egg/textures/DefaultMaterial_normal.jpeg");
-
-    let material_handle = materials.add(StandardMaterial {
-        base_color_texture: Some(egg_texture_handle),
-        metallic_roughness_texture: Some(egg_metallic_roughness_texture_handle),
-        normal_map_texture: Some(egg_normal_map_handle),
-        ..Default::default()
-    });
+    let egg_handle: Handle<Scene> = asset_server.load("egg/scene.gltf#Scene0");
 
     for i in 0..egg_count {
-        commands.spawn_bundle(PbrBundle {
-            mesh: egg_handle.clone(),
-            material: material_handle.clone(),
-            transform: Transform::from_xyz(
-                (i / egg_column_count) as f32 * egg_padding,
-                1.0,
-                (i % egg_column_count) as f32 * egg_padding,
-            ),
-            ..Default::default()
-        });
+        commands
+            .spawn_bundle((
+                Transform::from_xyz(
+                    (i / egg_column_count) as f32 * egg_padding,
+                    1.0,
+                    (i % egg_column_count) as f32 * egg_padding,
+                ),
+                GlobalTransform::identity(),
+            ))
+            .with_children(|builder| {
+                builder.spawn_scene(egg_handle.clone());
+            });
     }
 
     commands.spawn_bundle(PbrBundle {
